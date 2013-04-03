c17d03829bba790cd800f97c37d46063b2ada625
samuel santhosh <samuel@arrivusystems.com>
2013-04-03 18:16:16 +0530

    send mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,15 @@ class UsersController < ApplicationController
         redirect_to users_path, :notice => "User updated."
       else
        # Tell the UserMailer to send a welcome Email after save
-       UserEnableMailer.welcome_email(@user).deliver
-       redirect_to users_path
-       flash[:info]= "User updated and activation mail sent !"
-     end
-   else
+       begin
+         UserEnableMailer.welcome_email(@user).deliver
+         flash[:info]= "User updated and activation mail sent !"
+       rescue
+        flash[:info] = "There is some error while sending the email ..."
+      end
+      redirect_to users_path
+    end
+  else
 
     redirect_to users_path, :alert => "Unable to update user."
   end
@@ -42,16 +46,16 @@ def destroy
   end
 end
 
-  def add_users
-    if (params[:file] == nil)
-      params[:user_emails].split(",").each do |email|
-        @post = User.new(:email => email ,:password => "123456789" , :ac_id => params[:ac_id])
+def add_users
+  if (params[:file] == nil)
+    params[:user_emails].split(",").each do |email|
+      @post = User.new(:email => email ,:password => "123456789" , :ac_id => params[:ac_id])
 
-      end
-    else
-      require 'spreadsheet'
-      file = params[:file]
-      acid =params[:ac_id]
+    end
+  else
+    require 'spreadsheet'
+    file = params[:file]
+    acid =params[:ac_id]
     case File.extname(file.original_filename)
     when ".csv" then
       CSV.foreach(file.path) do |row|
@@ -63,28 +67,28 @@ end
       book = Spreadsheet.open(file.path, :col_sep => ',', :headers => true )
       sheet1 = book.worksheet 0
       sheet1.each do |row|
-      email =row[0]
-      @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
+        email =row[0]
+        @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
       end
     else raise "Unknown file type: #{file.original_filename}"
     end
-     
-    end
+    
+  end
 
-    respond_to do |format|
-      if @post.save
-        $error =nil
-        format.js
-        format.html { redirect_to (account_path(params[:ac_id])) }
-        flash[:success] = "user added to account successfully!!!!"
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        $error =nil
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-        format.html { redirect_to (account_path(params[:ac_id])) }
-        $error = @post
+  respond_to do |format|
+    if @post.save
+      $error =nil
+      format.js
+      format.html { redirect_to (account_path(params[:ac_id])) }
+      flash[:success] = "user added to account successfully!!!!"
+      format.xml  { render :xml => @post, :status => :created, :location => @post }
+    else
+      $error =nil
+      format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      format.html { redirect_to (account_path(params[:ac_id])) }
+      $error = @post
 
-      end
     end
   end
+end
 end
