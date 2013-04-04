0b888227e8206b283a3fdf8381f49734885f4368
manikkathir <manikkathir@arrivusystems.com>
2013-04-04 11:31:29 +0530

    user to account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,66 +17,111 @@ class UsersController < ApplicationController
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user], :as => :admin)
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
+      if params[:user][:is_active] == "0"
+        redirect_to users_path, :notice => "User updated."
+      else
+       # Tell the UserMailer to send a welcome Email after save
+       begin
+         UserEnableMailer.welcome_email(@user).deliver
+         flash[:info]= "User updated and activation mail sent !"
+       rescue
+        flash[:info] = "There is some error while sending the email ..."
+      end
+      redirect_to users_path
     end
+  else
+
+    redirect_to users_path, :alert => "Unable to update user."
   end
-  
-  def destroy
-    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
-      redirect_to users_path, :notice => "User deleted."
-    else
-      redirect_to users_path, :notice => "Can't delete yourself."
-    end
+end
+
+def destroy
+  authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+  user = User.find(params[:id])
+  unless user == current_user
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
+  else
+    redirect_to users_path, :notice => "Can't delete yourself."
   end
+end
 
-  def add_users
-    if (params[:file] == nil)
-      params[:user_emails].split(",").each do |email|
-        @post = User.new(:email => email ,:password => "123456789" , :ac_id => params[:ac_id])
+def add_users
+if(params[:file] != nil && params[:user_emails]!= "")
+  flash[:error] = "Please give any one input"
+  redirect_to (account_path(params[:ac_id]))
+elsif (params[:file] == nil && params[:user_emails]== "")
+  flash[:error] = "Please give any one input"
+  redirect_to (account_path(params[:ac_id]))
+else
 
-      end
-    else
-      require 'spreadsheet'
-      file = params[:file]
-      acid =params[:ac_id]
-    case File.extname(file.original_filename)
-    when ".csv" then
-      CSV.foreach(file.path) do |row|
-        email =row[0]
-        @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
-      end
-    when ".xls" || "xlsx" then
-      Spreadsheet.client_encoding = 'UTF-8'
-      book = Spreadsheet.open(file.path, :col_sep => ',', :headers => true )
-      sheet1 = book.worksheet 0
-      sheet1.each do |row|
-      email =row[0]
-      @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
-      end
-    else raise "Unknown file type: #{file.original_filename}"
-    end
-     
-    end
-
-    respond_to do |format|
+  if (params[:file] == nil)
+    params[:user_emails].split(",").each do |email|
+      @post = User.new(:email => email ,:password => "123456789" , :ac_id => params[:ac_id])
+      
       if @post.save
         $error =nil
-        format.js
-        format.html { redirect_to (account_path(params[:ac_id])) }
-        flash[:success] = "user added to account successfully!!!!"
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
+      end
+    end        
+  
+else
+  require 'spreadsheet'
+  file = params[:file]
+  acid =params[:ac_id]
+  case File.extname(file.original_filename)
+  when ".csv" then
+    CSV.foreach(file.path) do |row|
+      email =row[0]
+      @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
+      if @post.save
         $error =nil
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-        format.html { redirect_to (account_path(params[:ac_id])) }
-        $error = @post
-
       end
     end
+   
+  when ".txt" then
+     data=""
+  File.open(file.path).each_line do |line|
+    data += line
+end
+   data.split(",").each do |email|
+     
+      @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
+      if @post.save
+        $error =nil
+      end
+    end
+  when ".xls" || "xlsx" then
+    Spreadsheet.client_encoding = 'UTF-8'
+    book = Spreadsheet.open(file.path, :col_sep => ',', :headers => true )
+    sheet1 = book.worksheet 0
+    sheet1.each do |row|
+      email =row[0]
+      @post =User.new(:email => email ,:password => "123456789" , :ac_id => acid)
+      if @post.save
+        $error =nil
+      end
+    end
+  else raise "Unknown file type: #{file.original_filename}"
   end
+
+end
+
+respond_to do |format|
+  if @post.save
+    $error =nil
+    format.js
+    format.html { redirect_to (account_path(params[:ac_id])) }
+    flash[:success] = "user added to account successfully!!!!"
+    format.xml  { render :xml => @post, :status => :created, :location => @post }
+  else
+    $error =nil
+    format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+
+    format.html { redirect_to (account_path(params[:ac_id])) }
+    $error = @post
+
+  end
+end
+end
+end
 end
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,19 @@
 module AccountsHelper
+	def errormessage
+		 respond_to do |format|
+      if @post.save
+        $error =nil
+        format.js
+        format.html { redirect_to (account_path(params[:ac_id])) }
+        flash[:success] = "user added to account successfully!!!!"
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else
+        $error =nil
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+        format.html { redirect_to (account_path(params[:ac_id])) }
+        $error = @post
+
+      end
+    end
+	end
 end
