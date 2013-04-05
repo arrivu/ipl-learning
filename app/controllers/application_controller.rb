49f3e3e02dcd338b8df337d825fd6dc4fefbc322
samuel santhosh <samuel@arrivusystems.com>
2013-04-05 12:38:14 +0530

    devise before cinfirm! add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
- include ActiveMerchant::Billing::Integrations::ActionViewHelper
+  include ActiveMerchant::Billing::Integrations::ActionViewHelper
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
-
-
   def after_sign_in_path_for(resource_or_scope)
-  	if  params[:course_id] == "0" ||  params[:course_id] == nil
-  		root_path
-    else
-      @course = Course.find(params[:course_id])
-      new_comment_path(:commentable=>params[:course_id],:commentable_type=>"course")
-    end    
-  end
+    if current_user.has_role? :admin 
+     users_path
+   elsif  params[:course_id] == "0" ||  params[:course_id] == nil 
+    root_path    
+  else
+    @course = Course.find(params[:course_id])
+    new_comment_path(:commentable=>params[:course_id],:commentable_type=>"course")
+  end    
+end
 
 end
